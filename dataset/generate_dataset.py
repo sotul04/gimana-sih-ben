eedcda95c7735da6adab0e8afbac8b10d0e7c1dd
sotul04 <109208105+sotul04@users.noreply.github.com>
2024-12-15 15:37:44 +0700

    feat: add id3 model

--- dataset/generate_dataset.py
+++ dataset/generate_dataset.py
@@ -1,16 +1,24 @@
 import pandas as pd
-df_additional = pd.read_csv('dataset/train/additional_features_train.csv')
-df_basic = pd.read_csv('dataset/train/basic_features_train.csv')
-df_content = pd.read_csv('dataset/train/content_features_train.csv')
-df_flow = pd.read_csv('dataset/train/flow_features_train.csv')
-df_labels = pd.read_csv('dataset/train/labels_train.csv')
-df_time = pd.read_csv('dataset/train/time_features_train.csv')
 
-merged_df = pd.merge(df_additional, df_basic, on='id', how='inner')
+# Load the datasets
+df_flow = pd.read_csv('dataset/test/flow_features_test.csv')
+df_basic = pd.read_csv('dataset/test/basic_features_test.csv')
+df_content = pd.read_csv('dataset/test/content_features_test.csv')
+df_time = pd.read_csv('dataset/test/time_features_test.csv')
+df_additional = pd.read_csv('dataset/test/additional_features_test.csv')
+# df_labels = pd.read_csv('dataset/test/labels_test.csv')
+
+# Merge datasets on 'id'
+merged_df = pd.merge(df_flow, df_basic, on='id', how='inner')
 merged_df = pd.merge(merged_df, df_content, on='id', how='inner')
-merged_df = pd.merge(merged_df, df_flow, on='id', how='inner')
-merged_df = pd.merge(merged_df, df_labels, on='id', how='inner')
 merged_df = pd.merge(merged_df, df_time, on='id', how='inner')
+merged_df = pd.merge(merged_df, df_additional, on='id', how='inner')
+# merged_df = pd.merge(merged_df, df_labels, on='id', how='inner')
 
-merged_df.to_csv('dataset_train.csv', index=False)
-print('Successfully export merged dataset')
+# Reorder columns to move 'id' to the first position
+columns = ['id'] + [col for col in merged_df.columns if col != 'id']
+merged_df = merged_df[columns]
+
+# Export the merged dataset
+merged_df.to_csv('dataset_test.csv', index=False)
+print('Successfully exported merged dataset')
